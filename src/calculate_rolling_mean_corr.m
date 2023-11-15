da298998982f35a95b1f53b1e7b48944027a5b6b
Joseph Bradley <josephbradley16@googlemail.com>
2023-11-15 14:18:41 +0000

    Do not remove significant by default

--- src/calculate_rolling_mean_corr.m
+++ src/calculate_rolling_mean_corr.m
@@ -22,7 +22,7 @@ arguments
         dt (1, 1) double
         t_range (1, :) double
         options.CorrHandle (1, 1) function_handle = @(r) weighted_pearson_corrs(r, generate_expweights(dt, dt/3))
-        options.RemoveInsignificant (1, :) logical = true
+        options.RemoveInsignificant (1, :) logical = false
 end
 
 
@@ -48,11 +48,11 @@ for k = 1:length(t_range) %index for windows
     %correlation coefficients
     C = options.CorrHandle(X);
     C = force_symmetric(C); %bc MATLAB sometimes doesn't!
+    pvals = bootstrap_correlation_signifiance(X, C, 100, options.CorrHandle);
+    %calculate significant corrs 
+    significant = pvals < 0.05;
+    n_significant_corrs(k) = sum(utri_to_vec(significant));
     if options.RemoveInsignificant %remove anything not significant 
-        pvals = bootstrap_correlation_signifiance(X, C, 100, options.CorrHandle);
-        %calculate significant corrs 
-        significant = pvals < 0.05;
-        n_significant_corrs(k) = sum(utri_to_vec(significant));
         C(pvals >= 0.05) = 0;
         C = nearest_pos_semi_definite(C);
         if ~isreal(C)
